d4f1611a8a50902033f0ad73af87b2ae9ed44f7f
stbischof <stbischof@bipolis.org>
2018-06-27 23:42:01 +0200

    init: ./gradlew testOSGi

---
+++ org.bipolis.gradle.test/src/org/bipolis/gradle/test/ExampleIntegrationTest.java
@@ -0,0 +1,11 @@
+package org.bipolis.gradle.test;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class ExampleIntegrationTest {
+
+    @Test
+    public void testExample() {
+        assertTrue(true);
+    }
+}
